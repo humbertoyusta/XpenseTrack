b945adb7d30d70ff275a45ba7ec443833530b2af
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2024-05-10 12:01:44 +0200

    feat/close-transaction-activity-in-button

--- app/src/main/java/com/humbertoyusta/xpensetrack/ui/transaction/TopBar.kt
+++ app/src/main/java/com/humbertoyusta/xpensetrack/ui/transaction/TopBar.kt
@@ -1,6 +1,8 @@
 package com.humbertoyusta.xpensetrack.add_transaction.ui
 
+import android.app.Activity
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -8,14 +10,18 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun TopBar() {
+    val context = LocalContext.current
     Row(
         modifier = Modifier
             .fillMaxWidth()
@@ -25,11 +31,22 @@ fun TopBar() {
             .padding(16.dp)
     ) {
         Spacer(modifier = Modifier.weight(1f))
-        Icon(
-            imageVector = Icons.Rounded.Close,
-            contentDescription = "Close",
-            tint = MaterialTheme.colorScheme.onSurface,
-            modifier = Modifier.size(30.dp)
-        )
+        Button(
+            onClick = {
+                (context as? Activity)?.finish()
+            },
+            contentPadding = PaddingValues(0.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.background,
+            ),
+        ) {
+            Icon(
+                imageVector = Icons.Rounded.Close,
+                contentDescription = "Close",
+                tint = MaterialTheme.colorScheme.onSurface,
+                modifier = Modifier
+                    .size(30.dp)
+            )
+        }
     }
 }
