9dace9df93319fd2a572123bbd2f658e985aded0
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2024-05-13 10:02:57 +0200

    feat/add-rubik-font

---
+++ app/src/main/java/com/humbertoyusta/xpensetrack/ui/theme/FontFamily.kt
@@ -0,0 +1,15 @@
+package com.humbertoyusta.xpensetrack.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.humbertoyusta.xpensetrack.R
+
+val Rubik = FontFamily(
+    Font(R.font.rubik_regular),
+    Font(R.font.rubik_medium, FontWeight.Medium),
+    Font(R.font.rubik_bold, FontWeight.Bold),
+    Font(R.font.rubik_black, FontWeight.Black),
+    Font(R.font.rubik_light, FontWeight.Light),
+    Font(R.font.rubik_semibold, FontWeight.SemiBold),
+)
--- app/src/main/java/com/humbertoyusta/xpensetrack/ui/theme/Type.kt
+++ app/src/main/java/com/humbertoyusta/xpensetrack/ui/theme/Type.kt
@@ -2,28 +2,63 @@ package com.humbertoyusta.xpensetrack.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 20.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+    displayLarge = TextStyle(
+        fontFamily = Rubik,
+    ),
+    displayMedium = TextStyle(
+        fontFamily = Rubik,
+    ),
+    displaySmall = TextStyle(
+        fontFamily = Rubik,
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = Rubik,
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = Rubik,
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = Rubik,
     ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Rubik,
         fontWeight = FontWeight.Normal,
         fontSize = 28.sp,
         lineHeight = 32.sp,
         letterSpacing = 0.sp
     ),
+    titleMedium = TextStyle(
+        fontFamily = Rubik,
+    ),
+    titleSmall = TextStyle(
+        fontFamily = Rubik,
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = Rubik,
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = Rubik,
+    ),
+    bodySmall = TextStyle(
+        fontFamily = Rubik,
+    ),
+    labelLarge = TextStyle(
+        fontFamily = Rubik,
+    ),
+    labelMedium = TextStyle(
+        fontFamily = Rubik,
+    ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Rubik,
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         lineHeight = 18.sp,
